c7f45b6be07aba19e04315b9e7dca0510f6cb26d
yukichi0403 <bamboovalleyyuki@gmail.com>
2024-08-01 00:42:00 +0900

    modify for aux loss

--- src/model.py
+++ src/model.py
@@ -39,36 +39,36 @@ class CustomModel(nn.Module):
         self.encoder.classifier = nn.Identity()
         self.encoder.global_pool = nn.Identity()
         self.GeM = GeM(p=args.gem_p)
-        self.flatten = nn.Flatten()
         self.dropout_main = nn.ModuleList([
 			nn.Dropout(args.dropout) for _ in range(5)
 		]) #droupout augmentation
         self.linear_main = nn.Linear(self.classifier_in_features, args.num_classes)
 
-        if args.aux_loss_ratio is not None:
-            self.decoder_aux = nn.Flatten()
-            self.dropout_aux = nn.ModuleList([
-			nn.Dropout(args.dropout) for _ in range(5)
-		]) #droupout augmentation
-            self.linear_aux = nn.Linear(self.encoder.num_features, 4)
+        if self.aux_loss_features is not None:
+            self.aux_dropout = nn.ModuleList([nn.ModuleList([nn.Dropout(args.dropout) for _ in range(5)]) for _ in self.aux_loss_features])
+            self.aux_linear = nn.ModuleList([nn.Linear(self.encoder.num_features, outnum) for outnum in self.aux_loss_feature_outnum])
 
     def forward(self, images):
         out = self.encoder(images)
         out = self.GeM(out).flatten(1)
         if self.training:
-            main_out=0
+            main_out = 0
             for i in range(len(self.dropout_main)):
                 main_out += self.linear_main(self.dropout_main[i](out))
-            main_out = main_out/len(self.dropout_main)
-            if self.aux_loss_ratio is not None:
-                out_aux=0
-                for i in range(len(self.dropout_aux)):
-                    out_aux += self.linear_aux(self.dropout_aux[i](out))
-                out_aux = out_aux/len(self.dropout_aux)
-                return main_out, out_aux
+            main_out = main_out / len(self.dropout_main)
+
+            aux_outs = []
+            if self.aux_loss_features is not None:
+                for aux_dropout, aux_linear in zip(self.aux_dropout, self.aux_linear):
+                    out_aux = 0
+                    for i in range(len(aux_dropout)):
+                        out_aux += aux_linear(aux_dropout[i](out))
+                    out_aux = out_aux / len(aux_dropout)
+                    aux_outs.append(out_aux)
+                return main_out, aux_outs
         else:
             main_out = self.linear_main(out)
-        
+
         return main_out
     
 
