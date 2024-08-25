37b16326e23d1098ef3a8088f5d1b4c7f2562ab6
yukichi0403 <bamboovalleyyuki@gmail.com>
2024-08-25 13:23:19 +0900

    fix metadatamodel bug

--- src/model.py
+++ src/model.py
@@ -305,29 +305,27 @@ class CustomSwinModel(nn.Module):
         self.dropout_main = nn.ModuleList([nn.Dropout(args.dropout) for _ in range(5)])
         
         self.use_metadata = args.use_metadata_num is not None and args.use_metadata_num > 0
-        if self.use_metadata:
-            self.metadata_dim = args.metadata_dim  # New argument for metadata dimension
-            
+        if self.use_metadata:            
             self.metadata_encoder = nn.Sequential(
-                nn.Linear(args.use_metadata_num, self.metadata_dim * 4),
-                nn.BatchNorm1d(self.metadata_dim * 4),
+                nn.Linear(args.use_metadata_num, self.use_metadata_num * 4),
+                nn.BatchNorm1d(self.use_metadata_num * 4),
                 nn.SiLU(),
                 nn.Dropout(args.dropout),
-                nn.Linear(self.metadata_dim * 4, self.metadata_dim * 2),
-                nn.BatchNorm1d(self.metadata_dim * 2),
+                nn.Linear(self.use_metadata_num * 4, self.use_metadata_num * 2),
+                nn.BatchNorm1d(self.use_metadata_num * 2),
                 nn.SiLU(),
                 nn.Dropout(args.dropout),
-                nn.Linear(self.metadata_dim * 2, self.metadata_dim),
-                nn.BatchNorm1d(self.metadata_dim),
+                nn.Linear(self.use_metadata_num * 2, self.use_metadata_num),
+                nn.BatchNorm1d(self.use_metadata_num),
                 nn.SiLU(),
             )
             
             self.fusion = args.fusion_method  # New argument for fusion method
             if self.fusion == 'concat':
-                self.linear_main = nn.Linear(self.encoder.num_features + self.metadata_dim, args.num_classes)
+                self.linear_main = nn.Linear(self.encoder.num_features + self.use_metadata_num, args.num_classes)
             elif self.fusion == 'gated':
                 self.gate = nn.Sequential(
-                    nn.Linear(self.encoder.num_features + self.metadata_dim, self.encoder.num_features),
+                    nn.Linear(self.encoder.num_features + self.use_metadata_num, self.encoder.num_features),
                     nn.Sigmoid()
                 )
                 self.linear_main = nn.Linear(self.encoder.num_features, args.num_classes)
